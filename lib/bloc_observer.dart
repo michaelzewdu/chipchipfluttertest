346e355552b30cfe815cb9ffe4e96ea20eab7118
Michael Z <51156098+michaelzewdu@users.noreply.github.com>
2023-12-18 01:49:05 +0300

    feat: a simple bloc observer

---
+++ lib/bloc_observer.dart
@@ -0,0 +1,25 @@
+import 'package:bloc/bloc.dart';
+import 'package:flutter/foundation.dart';
+
+class SimpleBlocObserver extends BlocObserver {
+  const SimpleBlocObserver();
+
+  @override
+  void onTransition(
+    Bloc<dynamic, dynamic> bloc,
+    Transition<dynamic, dynamic> transition,
+  ) {
+    super.onTransition(bloc, transition);
+    if (kDebugMode) {
+      print(transition);
+    }
+  }
+
+  @override
+  void onError(BlocBase<dynamic> bloc, Object error, StackTrace stackTrace) {
+    if (kDebugMode) {
+      print(error);
+    }
+    super.onError(bloc, error, stackTrace);
+  }
+}
