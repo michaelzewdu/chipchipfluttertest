ce207ecc52d0a479c1de732a3c95140626c14cc8
Michael Z <51156098+michaelzewdu@users.noreply.github.com>
2023-12-18 03:24:10 +0300

    feat: home page UI structure built

--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -9,6 +9,7 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        backgroundColor: Color(0xF6F5F5FF),
         appBar: AppBar(
           title: const Column(
               crossAxisAlignment: CrossAxisAlignment.start,
@@ -38,23 +39,70 @@ class HomePage extends StatelessWidget {
                 return GridView.builder(
                     gridDelegate:
                         const SliverGridDelegateWithMaxCrossAxisExtent(
-                      maxCrossAxisExtent: 200,
-                    ),
+                            mainAxisExtent: 300,
+                            maxCrossAxisExtent: 200,
+                            mainAxisSpacing: 20,
+                            crossAxisSpacing: 8),
                     itemBuilder: (BuildContext context, int index) {
-                      return Container(
-                          child: Text(state.products!.data![index].name));
+                      return Padding(
+                        padding: const EdgeInsets.fromLTRB(16, 0, 8, 0),
+                        child: Container(
+                            decoration: BoxDecoration(
+                                color: Colors.white,
+                                borderRadius: BorderRadius.circular(15)),
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Image.network(
+                                  'https://productionchipchip.fra1.digitaloceanspaces.com/${state.products!.data![index].primary_image}',
+                                  height: 200,
+                                  fit: BoxFit.fill,
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.all(4.0),
+                                  child: Text(state.products!.data![index].name,
+                                      softWrap: true,
+                                      maxLines: 1,
+                                      style: const TextStyle(
+                                          fontWeight: FontWeight.w700,
+                                          fontSize: 17)),
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.all(4.0),
+                                  child: Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.spaceBetween,
+                                      children: [
+                                        Text(
+                                            '\$${state.products!.data![index].single_deal.original_price}',
+                                            style: const TextStyle(
+                                                fontWeight: FontWeight.w700,
+                                                fontSize: 17)),
+                                        TextButton(
+                                          onPressed: () {},
+                                          child: const Text("Add to cart",
+                                              style: TextStyle(
+                                                  color: Colors.pink,
+                                                  fontWeight: FontWeight.w700)),
+                                        )
+                                      ]),
+                                )
+                              ],
+                            )),
+                      );
                     },
                     itemCount: state.products!.data!.length);
               case ProductsStatus.initial:
-                return Column(children: [
-                  const Center(child: CircularProgressIndicator()),
-                  TextButton(
-                    onPressed: () {
-                      context.read<ProductsBloc>().add(ProductsFetched());
-                      print('Retrying');
-                    },
-                    child: Text('Try again'),
-                  )
+                return const Column(children: [
+                  Center(child: CircularProgressIndicator()),
+                  // TextButton(
+                  //   onPressed: () {
+                  //     context.read<ProductsBloc>().add(ProductsFetched());
+                  //     print('Retrying');
+                  //   },
+                  //   child: Text('Try again'),
+                  // )
                 ]);
             }
           },
